99801cc2f1d5080c13fca061af930e458f062f48
Ashu Kumar <95637428+Ashu-kumar40@users.noreply.github.com>
2023-10-20 11:35:25 +0000

    Added Lock/Unlock functionality

--- Basic_code_editor/script.js
+++ Basic_code_editor/script.js
@@ -5,7 +5,13 @@ const codeTextarea = document.getElementById('code');
 
 // Copy button functionality
 copyButton.addEventListener('click', () => {
-  const code = codeTextarea.value;
-  navigator.clipboard.writeText(code);
-  alert('Code copied to clipboard');
+    const code = codeTextarea.value;
+    navigator.clipboard.writeText(code);
+    alert('Code copied to clipboard');
+});
+
+// Lock button functionality
+lockButton.addEventListener('click', () => {
+    const isLocked = codeTextarea.disabled;
+    codeTextarea.disabled = !isLocked;
 });
