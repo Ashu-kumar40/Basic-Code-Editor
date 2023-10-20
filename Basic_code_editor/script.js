dbc9429bd39a02edeed9dd8c7df22778fec480f4
Ashu Kumar <95637428+Ashu-kumar40@users.noreply.github.com>
2023-10-20 11:37:13 +0000

    Saving file to local device functionality added

--- Basic_code_editor/script.js
+++ Basic_code_editor/script.js
@@ -15,3 +15,17 @@ lockButton.addEventListener('click', () => {
     const isLocked = codeTextarea.disabled;
     codeTextarea.disabled = !isLocked;
 });
+
+// Saving the code in local device
+saveButton.addEventListener('click', () => {
+    const textToSave = codeTextarea.value; // Get the text from the textarea
+    const blob = new Blob([textToSave], { type: 'text/plain' }); // Create a blob
+    // blob is an object that represents a chuck of data in a specific format
+    const url = URL.createObjectURL(blob); // Create a URL where our blob is hosted
+  
+    const a = document.createElement('a'); // Create a link element
+    a.href = url; // Set the href of our link
+    a.download = 'code.txt'; // Set the download attribute of our link
+    a.click(); // Click on the link
+    URL.revokeObjectURL(url); // Free up the memory
+  });
